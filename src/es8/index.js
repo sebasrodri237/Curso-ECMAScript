f931b3a57b4915a95350e7aa7580b19bd5efb4f3
sebasrodri237 <sebasplay2@hotmail.com>
2020-06-07 00:26:05 -0500

    clase 8 - ES8 Async Await

--- src/es8/index.js
+++ src/es8/index.js
@@ -25,3 +25,34 @@ console.log(string.padEnd(12,'-------'));
 //Agregar caracteres al inicio/final de un string indicando el numero maximo
 //de caracteres que tendra el string y la cadena que deseamos agregar
 // FIN CLASE 7//
+
+// CLASE 8 Implementaciones en ES8 ASYNC AWAIT//
+
+const helloWorld = () =>{
+
+    return new Promise((resolve, reject) =>{
+        (true)//Esto es un if ternario
+            ? setTimeout(() => resolve('Hello World'), 3000)
+            : reject(new Error('Test Error'))
+    }) 
+}
+//Creamos una promesa
+const helloAsync = async () =>{
+    const hello = await helloWorld();
+    console.log(hello);
+}
+
+helloAsync();
+//Propuesta de uso para el async await
+const anotherFunction = async () =>{
+    try{
+        const hello = await helloWorld();
+        console.log(hello);
+    }catch(error){
+        console.log(error);
+    }
+}
+anotherFunction();
+
+//Otra propuesta de uso para el async await
+// FIN CLASE 8//
